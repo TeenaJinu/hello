224b9974078cdcebf70dae762a6e3e7fd4cb509d
Teena Thomas <tinathms@gmail.com>
2021-02-02 18:45:52 -0500

    fourth commit on feb2

---
+++ places.py
@@ -0,0 +1,9 @@
+favPlaces= ['London','Switzerland','Dubai','Sweden','Austria','Australia','Nepal']
+print (favPlaces)
+print(sorted(favPlaces))
+print (favPlaces)
+print(sorted(favPlaces,reverse=True))
+favPlaces.reverse()   
+print(favPlaces)
+favPlaces.sort()
+print(favPlaces)
